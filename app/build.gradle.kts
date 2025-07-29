29d8498677dc73812a74893ab9134466357c6cb8
chilbi <13232158717@163.com>
2025-07-29 19:58:02 +0800

    v0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.pcrclicker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
